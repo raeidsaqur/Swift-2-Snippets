1f3fcb39c54886b2e5fe56d64c10923823cb7d42
raeidsaqur <raeidsaqur@gmail.com>
2016-03-27 19:36:29 -0400

    async singleton todo

---
+++ singleton.swift
@@ -0,0 +1,15 @@
+---
+title: "Shared Singleton"
+summary: "Definition for a singleton class"
+completion-scope: Class Implementation
+---
+
+final class <#Singleton#> {
+
+    static let sharedInstance = <#Singleton#>()
+
+    private init() {
+        <#code#>
+    }
+    
+}
